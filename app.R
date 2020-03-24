251ddde99128811cb4f5149444d50304b804d43a
roboton <roberton@gmail.com>
2020-03-24 03:56:52 -0700

    - add link to static dashboard - add min_stat select - download option - remove crappy dynamic ui options - use genPlotComps shorthand function

--- app.R
+++ app.R
@@ -5,8 +5,8 @@ library(plotly)
 library(DT)
 
 source("covidcomp_lib.R")
-min_us <- 5
-min_global <- 10
+min_us <- 1
+min_global <- 1
 
 # pull in data
 jhu <- fetchPrepJhuData()
@@ -34,37 +34,41 @@ ui <- fluidPage(
   tags$a(
     href = "mailto:roberton@gmail.com",
     target = "_blank", "[contact]"),
+  tags$a(
+    href = "https://robon.shinyapps.io/covidcomp/covidcomp_static.html",
+    target = "_blank", "[too slow?]"),
   sidebarLayout(
     sidebarPanel(
       # data options
-      sliderInput("min_total",
-                  "initial number of deaths:",
-                  min = 1,
-                  max = 50,
-                  value = 10),
-      sliderInput("max_days_since",
-                  "days since nth death",
-                  min = 5,
-                  max = 90,
-                  value = 30),
+      selectInput("min_stat", "metric to compare by:",
+                  c("Deaths" = "deaths", "Confirmed cases" = "confirmed")),
+      numericInput("min_thresh",
+                   "initial number of deaths/cases:",
+                   min = 0, value = min_global),
+      numericInput("max_days_since",
+                   "days since initial number of deaths/cases:",
+                   min = 0, value = 30),
       # plot options
+      checkboxInput("per_capita",
+                    "Counts per capita", value = TRUE),
       checkboxInput("smooth_plots",
                     "Smooth plot values", value = TRUE), 
       checkboxInput("scale_to_fit",
                     "Scale to fit", value = TRUE), 
-      width = 2
-    ),
+      width = 1),
     mainPanel(
       tabsetPanel(
         id = "plotTabs", type = "tabs",
         tabPanel(
           "Global", value = "Global",
           plotlyOutput(
-            "compPlot", width = "100%", height = "1600px")),
+            "compPlot", width = "100%", height = "1600px"),
+            downloadButton("downloadGlobalData", "download")),
         tabPanel(
           "US", value = "US",
           plotlyOutput(
-            "compPlotUS", width = "100%", height = "1600px"))
+            "compPlotUS", width = "100%", height = "1600px"),
+            downloadButton("downloadUSData", "download"))
         # tabPanel(
         #   "Data", dataTableOutput("compData"),
         #   downloadButton("downloadCompData", "Download"))
@@ -75,48 +79,50 @@ ui <- fluidPage(
  
 server <- function(input, output, session) {
   shinyjs::runjs("toggleCodePosition();")
-  observe({
-    updateSliderInput(
-      session, "min_total",
-      value = {if (input$plotTabs == "US") min_us else min_global})
-  })
-  
-  output$compPlot <- renderPlotly({
-    withProgress(message = 'Making plot', value = 0, {
-      jhu %>% genCompData(min_total = input$min_total) %>%
-        # filter plots
-        filter(!(stat %in% c("cfr", "crr") & value_type == "double_days")) %>%
-        plotComps(min_total = input$min_total,
-                  max_days_since = input$max_days_since,
-                  smooth_plots = input$smooth_plots,
-                  scale_to_fit = input$scale_to_fit)
-    })
-  })
-  output$compPlotUS <- renderPlotly({
-    withProgress(message = 'Making plot', value = 0, {
-      covtrack %>%
-        genCompData(geo_level = "state", min_stat = "death",
-                    min_total = input$min_total) %>%
-        # filter plots
-        filter(!stat %in% c("negative", "pending")) %>%
-        filter(!(endsWith(stat, "r") & value_type == "double_days")) %>%
-        plotComps(min_total = input$min_total,
-                  max_days_since = input$max_days_since,
-                  smooth_plots = input$smooth_plots,
-                  scale_to_fit = input$scale_to_fit)
-    })
-  })
-  # output$compData <- renderDataTable({
-  #   joined %>% genCompData(min_total = input$min_total)
+  # observe({
+  #   updateNumericInput(
+  #     session, "min_thresh",
+  #     value = {if (input$plotTabs == "US") min_us else min_global})
   # })
-  # output$downloadCompData <- downloadHandler(
-  #   filename = function() {
-  #     paste0("covid-comp-data-", Sys.Date(), ".csv")
-  #   },
-  #   content = function(file) {
-  #     write_csv(joined %>% genCompData(min_total = input$min_total), file)
-  #   }
-  # )
+  
+  suppressWarnings(output$compPlot <- renderPlotly({
+    jhu %>% genPlotComps(geo_level = "country", min_thresh = input$min_thresh,
+                         per_capita = input$per_capita,
+                         min_stat = input$min_stat,
+                         max_days_since = input$max_days_since,
+                         smooth_plots = input$smooth_plots,
+                         scale_to_fit = input$scale_to_fit) }))
+  suppressWarnings(output$compPlotUS <- renderPlotly({
+    covtrack %>%
+      genPlotComps(geo_level = "state", min_thresh = input$min_thresh,
+                   per_capita = input$per_capita,
+                   min_stat = input$min_stat,
+                   max_days_since = input$max_days_since,
+                   smooth_plots = input$smooth_plots,
+                   scale_to_fit = input$scale_to_fit) }))
+  # output$compData <- renderDataTable({
+  #   joined %>% genCompData(min_thresh = input$min_thresh)
+  # })
+  output$downloadGlobalData <- downloadHandler(
+    filename = function() {
+      paste0("covid-global-comp-data-", Sys.Date(), ".csv")
+    },
+    content = function(file) {
+      write_csv(jhu %>% genCompData(
+        geo_level = "country", min_thresh = input$min_thresh,
+        per_capita = input$per_capita, min_stat = input$min_stat), file)
+    }
+  )
+  output$downloadUSData <- downloadHandler(
+    filename = function() {
+      paste0("covid-us-comp-data-", Sys.Date(), ".csv")
+    },
+    content = function(file) {
+      write_csv(covtrack %>% genCompData(
+        geo_level = "state", min_thresh = input$min_thresh,
+        per_capita = input$per_capita, min_stat = input$min_stat), file)
+    }
+  )
 }
 
 shinyApp(ui = ui, server = server)
